dfcee84ab932a13ecdaab3cfe1531110caf0e7f6
Keerthana <keert_207@yahoo.com>
2026-03-19 23:17:05 +0530

    update app for register page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './pages/Profile'
+import Register from './pages/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Profile />
+      {/* <Profile /> */}
+      <Register />
     </>
   )
 }
