65b97ee24a519578f4d86a9f9b955c9fc6391920
Keerthana <keert_207@yahoo.com>
2026-03-20 18:01:18 +0530

    login and profile page with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Profile from './pages/Profile'
 import Register from './pages/Register'
+import Login from './pages/Login'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Profile /> */}
-      <Register />
+    <Routes>
+      <Route path='/register' element={<Register />}/>
+      <Route path='/login' element={<Login />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
     </>
   )
 }
